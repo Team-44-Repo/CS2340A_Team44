887a610c0cc42d23b8711b08834d0d5d05d674d1
betugg <113387268+betugg@users.noreply.github.com>
2023-09-22 16:39:21 -0400

    Adding difficulty UI and functionality

--- CS2340A/app/src/main/java/com/example/cs2340a/gameActivity.java
+++ CS2340A/app/src/main/java/com/example/cs2340a/gameActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class gameActivity extends AppCompatActivity {
 
     Button tempEndButton;
+    private TextView healthTextView;
+    private int difficulty; // Establishes difficulty
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,10 @@ public class gameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         tempEndButton = (Button) findViewById(R.id.tempEndButton_id);
+        healthTextView = findViewById(R.id.healthTextView);
+        // Connects difficulty value from iniConfig Screen
+        difficulty = getIntent().getIntExtra("difficulty", 1);
+        healthTextView.setText("Health: " + difficulty);
 
         tempEndButton.setOnClickListener(new View.OnClickListener() {
             @Override
--- CS2340A/app/src/main/java/com/example/cs2340a/initialConfigurationActivity.java
+++ CS2340A/app/src/main/java/com/example/cs2340a/initialConfigurationActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class initialConfigurationActivity extends AppCompatActivity {
 
@@ -22,7 +23,26 @@ public class initialConfigurationActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                RadioGroup difficultyRadioGroup = findViewById(R.id.difficultyRadioGroup_id);
+                int difficulty;
+
+                switch (difficultyRadioGroup.getCheckedRadioButtonId()) {
+                    case R.id.radioEasy_id:
+                        difficulty = 20;
+                        break;
+                    case R.id.radioMedium_id:
+                        difficulty = 10;
+                        break;
+                    case R.id.radioHard_id:
+                        difficulty = 5;
+                        break;
+                    default:
+                        difficulty = 20;
+                        break;
+                }
+
                 Intent intent = new Intent(initialConfigurationActivity.this, gameActivity.class);
+                intent.putExtra("difficulty", difficulty);
                 startActivity(intent);
 
             }
