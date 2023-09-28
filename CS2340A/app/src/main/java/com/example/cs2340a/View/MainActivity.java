ab816a65f2696408e322e52a2387340807a62475
betugg <113387268+betugg@users.noreply.github.com>
2023-09-28 11:37:27 -0400

    Started MVVM organization

--- CS2340A/app/src/main/java/com/example/cs2340a/View/MainActivity.java
+++ CS2340A/app/src/main/java/com/example/cs2340a/View/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.cs2340a;
+package com.example.cs2340a.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.cs2340a.R;
 
 public class MainActivity extends AppCompatActivity {
 
