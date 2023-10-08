2a32b1e47fe7f052a51eda30c81c2b2fe22bc36e
CC-734 <clarecotter7@gmail.com>
2023-10-08 18:09:27 -0400

    logic for leaderboard singleton class and singleton viewmodel

--- CS2340A/app/src/main/java/com/example/cs2340a/dungenCrawler/model/Leaderboard.java
+++ CS2340A/app/src/main/java/com/example/cs2340a/dungenCrawler/model/Leaderboard.java
@@ -1,4 +1,54 @@
 package com.example.cs2340a.dungenCrawler.model;
+import java.util.ArrayList;
 
 public class Leaderboard {
+    //singleton class with getters and setters
+    private String[] names;
+    private int[] scores;
+
+    private static Leaderboard instance;
+
+    private Leaderboard(String[] names, int[] scores) {
+        if (names == null) {
+            this.names = new String[5];
+        }
+        //if scores array is null, make every score 0 since int can't be null
+        if (scores == null) {
+            this.scores = new int[5];
+            for (int i = 0; i < 5; i ++) {
+                scores[i] = 0;
+            }
+        } else {
+            this.names = names;
+            this.scores = scores;
+        }
+    }
+    private Leaderboard() {
+        this(null, null);
+    }
+
+    //one instance of leaderboard
+    public static synchronized Leaderboard getInstance() {
+        if (instance == null) {
+            instance = new Leaderboard();
+        }
+        return instance;
+    }
+
+    //getters and setters
+    public String[] getNames() {
+        return names;
+    }
+
+    public int[] getScores() {
+        return scores;
+    }
+
+    public void setScore(int score, int index) {
+        scores[index] = score;
+    }
+
+    public void setName(String name, int index) {
+        names[index] = name;
+    }
 }
