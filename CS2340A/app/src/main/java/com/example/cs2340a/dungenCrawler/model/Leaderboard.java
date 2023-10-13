3ba973efc031b606b139de014c7a77ea3da53cff
betugg <113387268+betugg@users.noreply.github.com>
2023-10-13 09:48:23 -0400

    Finishing Leaderboard

--- CS2340A/app/src/main/java/com/example/cs2340a/dungenCrawler/model/Leaderboard.java
+++ CS2340A/app/src/main/java/com/example/cs2340a/dungenCrawler/model/Leaderboard.java
@@ -1,15 +1,19 @@
 package com.example.cs2340a.dungenCrawler.model;
+import java.util.Arrays;
 
 public class Leaderboard {
     //singleton class with getters and setters
-    private String[] names;
-    private int[] scores;
+    private static String[] names = new String[6];
+    private static int[] scores = new int[6];
 
     private static Leaderboard instance;
 
     private Leaderboard(String[] names, int[] scores) {
         if (names == null) {
             this.names = new String[5];
+            for (int i = 0; i < 5; i++) {
+                names[i] = "XXXX";
+            }
         }
         //if scores array is null, make every score 0 since int can't be null
         if (scores == null) {
@@ -22,14 +26,14 @@ public class Leaderboard {
             this.scores = scores;
         }
     }
-    private Leaderboard() {
-        this(null, null);
-    }
+//    private Leaderboard() {
+//        this(null, null);
+//    }
 
     //one instance of leaderboard
     public static synchronized Leaderboard getInstance() {
         if (instance == null) {
-            instance = new Leaderboard();
+            instance = new Leaderboard(names, scores);
         }
         return instance;
     }
@@ -43,11 +47,72 @@ public class Leaderboard {
         return scores;
     }
 
-    public void setScore(int score, int index) {
-        scores[index] = score;
-    }
+    public void setScore(int score, int index) { scores[index] = score; }
 
     public void setName(String name, int index) {
         names[index] = name;
+    }
+    public void addScores(int score, String name) {
+        for (int i = 0; i < 5; i++) {
+            if (scores[i] == 0) {
+                scores[i] = score;
+                names[i] = name;
+                break;
+            } else if (scores[i] > score) {
+                continue;
+            } else if (scores[i] < score) {
+                if (scores[i + 1] == 0) {
+                    scores[i + 1] = scores[i];
+                    names[i + 1] = names[i];
+                    scores[i] = score;
+                    names[i] = name;
+                    break;
+                } else {
+                    int count = i;
+                    scores[5] = score;
+                    int placeholder;
+                    String placeholderName;
+                    while (count + 1 <= 5) {
+                        System.out.println("Starting while loop...");
+                        if (scores[count] < scores[count + 1]) {
+                            placeholder = scores[count];
+                            placeholderName = names[count];
+                            scores[count] = scores[count + 1];
+                            names[count] = names[count + 1];
+                            scores[count + 1] = placeholder;
+                            names[count + 1] = placeholderName;
+                            count++;
+                        } else {
+                            count++;
+                        }
+                    }
+//                    names[count] = name;
+                    break;
+                }
+            }
+        }
+        //        if ((scores[index] != 0) {
+//            int placeholder;
+//            int placeholder2;
+//            for (int i = index + 1; i < 5; i++) {
+//                if (scores[i] != 0 && scores[i] != placeholder) {
+//                    placeholder = scores[i];
+//                    if (scores[i + 1] != 0) {
+//                        placeholder2 = scores[i + 1];
+//                        scores[i + 1] = placeholder;
+//                    }
+//                }
+//            }
+//        } else {
+//            scores[index] = score;
+//        }
+//        if (scores[index] != 0) {
+//            scores[6] = score;
+//            System.out.println("Unsorted: " + scores);
+//            Arrays.sort(scores);
+//            System.out.println("Sorted: " + scores);
+//        } else {
+//            scores[index] = score;
+//        }
     }
 }
--- CS2340A/app/src/main/java/com/example/cs2340a/dungenCrawler/viewModel/LeaderboardViewModel.java
+++ CS2340A/app/src/main/java/com/example/cs2340a/dungenCrawler/viewModel/LeaderboardViewModel.java
@@ -10,20 +10,25 @@ import android.widget.TextView;
 
 import com.example.cs2340a.R;
 
+import com.example.cs2340a.dungenCrawler.model.Leaderboard;
 import com.example.cs2340a.dungenCrawler.model.Player;
 
 public class LeaderboardViewModel extends AppCompatActivity {
 
-    //public Leaderboard leaderboard;
+    private Leaderboard leaderboard;
 
-    private long timeLeft;
+    private int timeLeft;
 
     private Player player;
 
-    //private String[] names;
+    private String[] names;
 
-    //private long[] scores;
-    private TextView scoreboard;
+    private int[] scores;
+    private TextView score1;
+    private TextView score2;
+    private TextView score3;
+    private TextView score4;
+    private TextView score5;
 
     private TextView playerNameTV;
 
@@ -38,18 +43,31 @@ public class LeaderboardViewModel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard_view_model);
-        //leaderboard = Leaderboard.getInstance();
+
+        leaderboard = Leaderboard.getInstance();
         playerNameTV = findViewById(R.id.playerNameDisplayEnd_id);
-        scoreboard = findViewById(R.id.score_id);
-        timeLeft = getIntent().getLongExtra("timeLeftInMilliseconds", timeLeft);
+        score1 = findViewById(R.id.score_id);
+        score2 = findViewById(R.id.score_id2);
+        score3 = findViewById(R.id.score_id3);
+        score4 = findViewById(R.id.score_id4);
+        score5 = findViewById(R.id.score_id5);
 
-
+        timeLeft = (int) getIntent().getLongExtra("timeLeftInMilliseconds", timeLeft);
         player = getIntent().getParcelableExtra("player");
+
+        leaderboard.addScores(timeLeft, player.getPlayerName());
+        scores = leaderboard.getScores();
+        System.out.println("Scores after Add: " + scores);
+        names = leaderboard.getNames();
+        System.out.println("Names after Add: " + names);
+
+//        score1.setText("" + player.getPlayerName() + ", " + timeLeft);
         playerNameTV.setText(player.getPlayerName());
-
-
-
-        scoreboard.setText("" + player.getPlayerName() + ", " + timeLeft);
+        score1.setText(names[0] + ", " + scores[0]);
+        score2.setText(names[1] + ", " + scores[1]);
+        score3.setText(names[2] + ", " + scores[2]);
+        score4.setText(names[3] + ", " + scores[3]);
+        score5.setText(names[4] + ", " + scores[4]);
 
         Button restartBtn = (Button) findViewById(R.id.restartBtn);
 
