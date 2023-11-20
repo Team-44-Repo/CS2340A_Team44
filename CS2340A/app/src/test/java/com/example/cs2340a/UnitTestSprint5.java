a88f713fdf0dfea5182b6dfc2fccc55b413630d7
CC-734 <clarecotter7@gmail.com>
2023-11-20 11:22:36 -0500

    sprint 5 unit tests

--- CS2340A/app/src/test/java/com/example/cs2340a/UnitTestSprint5.java
+++ CS2340A/app/src/test/java/com/example/cs2340a/UnitTestSprint5.java
@@ -1,9 +1,12 @@
 package com.example.cs2340a;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import com.example.cs2340a.dungenCrawler.model.DifficultyEnum;
 import com.example.cs2340a.dungenCrawler.model.GameConfig;
+import com.example.cs2340a.dungenCrawler.model.HeartPower;
+import com.example.cs2340a.dungenCrawler.model.PowerUp;
 
 import org.junit.Test;
 
@@ -18,5 +21,20 @@ public class UnitTestSprint5 {
     public void setDifficultyMedium() { // Elizabeth Grace Tuggle
         GameConfig.setDifficulty(DifficultyEnum.MEDIUM);
         assertEquals(DifficultyEnum.MEDIUM, GameConfig.getDifficulty());
+    }
+
+    @Test       //Clare Cotter
+    public void addPowerUp() {
+        PowerUp power = new PowerUp();
+        GameConfig.setPowerUp(power);
+        assertNotNull(GameConfig.getPowerUp());
+    }
+
+    @Test       //Clare Cotter
+    public void activePowerUp() {
+        PowerUp power = new PowerUp();
+        power.setActive(true);
+        GameConfig.setPowerUp(power);
+        assertEquals(true, power.isActive());
     }
 }
